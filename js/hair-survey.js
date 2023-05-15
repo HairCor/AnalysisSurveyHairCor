e821babec006daae753f7266e3560f729c153289
JJStephens <siahstephens@gmail.com>
2023-05-15 17:07:27 -0600

    fixed for site

---
+++ js/hair-survey.js
@@ -0,0 +1,229 @@
+console.log("form.js loaded");
+
+//jQuery time
+var current_fs, next_fs, previous_fs; //fieldsets
+var current_section = 0;
+var left, opacity, scale; //fieldset properties which we will animate
+var animating; //flag to prevent quick multi-click glitches
+
+// copied from https://stackoverflow.com/questions/57939882/saving-form-submitted-data-to-csv-file-using-javascript-jquery
+function clearForm() {
+  document.getElementById("msform").reset();
+}
+
+function submitForm() {
+  var formData = $("form").serializeArray();
+  let csv = "data:text/csv;charset=utf-8,"; // accept data as CSV
+
+  const columnTitles = [
+    "Hair Thickness",
+    "Minutes to Dry",
+    "Length (Inches)",
+    "Bleach",
+    "Color",
+    "Tight Style",
+    "Hair Loss",
+    "Natural Hair Color",
+    "Life Events",
+    "Hair Goals",
+    "Disliked Products",
+    "Addition Information",
+  ];
+  csv += columnTitles.join(";") + "\n";
+
+  formData.forEach(function (item) {
+    csv += item.value + ";"; // concat form value on csv var and add ; to create columns (you can change to , if want)
+  });
+
+  var encodedUri = encodeURI(csv);
+
+  // if you want to download
+  var downloadLink = document.createElement("a");
+  downloadLink.setAttribute("download", "FILENAME.csv");
+  downloadLink.setAttribute("href", encodedUri);
+  document.body.appendChild(downloadLink); // Required for FF
+  downloadLink.click();
+  downloadLink.remove();
+
+  clearForm();
+}
+
+$(document).ready(function () {
+  $(".next-section").click(function () {
+    console.log("current_fs", current_fs);
+
+    if (animating) return false;
+    animating = true;
+
+    current_fs = $(this).parent();
+    next_fs = $(this).parent().next();
+
+    console.log("current_section", current_section);
+    current_section += 1;
+    console.log("moved to section", current_section);
+
+    //activate next step on progressbar using the index of next_fs
+    $("#progressbar li").eq(current_section).addClass("active");
+
+    //show the next fieldset
+    next_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate(
+      { opacity: 0 },
+      {
+        step: function (now, mx) {
+          //as the opacity of current_fs reduces to 0 - stored in "now"
+          //1. scale current_fs down to 80%
+          scale = 1 - (1 - now) * 0.2;
+          //2. bring next_fs from the right(50%)
+          left = now * 50 + "%";
+          //3. increase opacity of next_fs to 1 as it moves in
+          opacity = 1 - now;
+          current_fs.css({
+            transform: "scale(" + scale + ")",
+            position: "absolute",
+          });
+          next_fs.css({ left: left, opacity: opacity });
+        },
+        duration: 800,
+        complete: function () {
+          current_fs.hide();
+          animating = false;
+        },
+        //this comes from the custom easing plugin
+        // easing: "easeInOutBack",
+      }
+    );
+  });
+
+  $(".previous-section").click(function () {
+    if (animating) return false;
+    animating = true;
+
+    console.log("current_fs", current_fs);
+
+    current_fs = $(this).parent();
+    previous_fs = $(this).parent().prev();
+
+    //de-activate current step on progressbar
+    $("#progressbar li").eq(current_section).removeClass("active");
+
+    console.log("current_section", current_section);
+    current_section -= 1;
+    console.log("moved to section", current_section);
+
+    //show the previous fieldset
+    previous_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate(
+      { opacity: 0 },
+      {
+        step: function (now, mx) {
+          //as the opacity of current_fs reduces to 0 - stored in "now"
+          //1. scale previous_fs from 80% to 100%
+          scale = 0.8 + (1 - now) * 0.2;
+          //2. take current_fs to the right(50%) - from 0%
+          left = (1 - now) * 50 + "%";
+          //3. increase opacity of previous_fs to 1 as it moves in
+          opacity = 1 - now;
+          current_fs.css({ left: left });
+          previous_fs.css({
+            transform: "scale(" + scale + ")",
+            opacity: opacity,
+          });
+        },
+        duration: 800,
+        complete: function () {
+          current_fs.hide();
+          animating = false;
+        },
+        //this comes from the custom easing plugin
+        // easing: "easeInOutBack",
+      }
+    );
+  });
+
+  $(".next").click(function () {
+    console.log("current_fs", current_fs);
+    if (animating) return false;
+    animating = true;
+
+    current_fs = $(this).parent();
+    next_fs = $(this).parent().next();
+
+    //show the next fieldset
+    next_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate(
+      { opacity: 0 },
+      {
+        step: function (now, mx) {
+          //as the opacity of current_fs reduces to 0 - stored in "now"
+          //1. scale current_fs down to 80%
+          scale = 1 - (1 - now) * 0.2;
+          //2. bring next_fs from the right(50%)
+          left = now * 50 + "%";
+          //3. increase opacity of next_fs to 1 as it moves in
+          opacity = 1 - now;
+          current_fs.css({
+            transform: "scale(" + scale + ")",
+            position: "absolute",
+          });
+          next_fs.css({ left: left, opacity: opacity });
+        },
+        duration: 800,
+        complete: function () {
+          current_fs.hide();
+          animating = false;
+        },
+        //this comes from the custom easing plugin
+        // easing: "easeInOutBack",
+      }
+    );
+  });
+
+  $(".previous").click(function () {
+    console.log("current_fs", current_fs);
+    if (animating) return false;
+    animating = true;
+
+    current_fs = $(this).parent();
+    previous_fs = $(this).parent().prev();
+
+    //show the previous fieldset
+    previous_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate(
+      { opacity: 0 },
+      {
+        step: function (now, mx) {
+          //as the opacity of current_fs reduces to 0 - stored in "now"
+          //1. scale previous_fs from 80% to 100%
+          scale = 0.8 + (1 - now) * 0.2;
+          //2. take current_fs to the right(50%) - from 0%
+          left = (1 - now) * 50 + "%";
+          //3. increase opacity of previous_fs to 1 as it moves in
+          opacity = 1 - now;
+          current_fs.css({ left: left });
+          previous_fs.css({
+            transform: "scale(" + scale + ")",
+            opacity: opacity,
+          });
+        },
+        duration: 800,
+        complete: function () {
+          current_fs.hide();
+          animating = false;
+        },
+        //this comes from the custom easing plugin
+        // easing: "easeInOutBack",
+      }
+    );
+  });
+
+  $(".submit").click(function () {
+    submitForm();
+    window.location.href = "https://josiahstephens.github.io/Bellu-Interview/completed.html";
+    return false;
+  });
+});
