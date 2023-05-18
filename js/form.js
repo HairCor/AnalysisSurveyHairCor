358267fe7d763dabeef360a02cfea1c0cbee5986
JJStephens <siahstephens@gmail.com>
2023-05-18 15:07:30 -0600

    made fields required and enabled next buttons

--- js/form.js
+++ js/form.js
@@ -14,6 +14,24 @@ function submitForm() {
   clearForm();
 }
 
+// validate email
+function validateEmail(email) {
+  const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return regex.test(email);
+}
+
+// validate age
+function validateAge(ageText) {
+  const regex = /^(?:1[0-1][0-9]|120|[1-9][0-9]?)$/;
+  return regex.test(ageText);
+}
+
+// validate zipcode
+function validateZipCode(zipcodeText) {
+  const regex = /^[0-9]{5}$/;
+  return regex.test(zipcodeText);
+}
+
 $(document).ready(function () {
   $(".next-section").click(function () {
     // console.log("current_fs", current_fs);
